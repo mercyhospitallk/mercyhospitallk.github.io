3bbed8ae5c0fc338f1d707b8dcb1e658270782af
Hiras <hirasharis03@gmail.com>
2019-10-29 12:59:42 +0530

    map final

--- js/google-map.js
+++ js/google-map.js
@@ -46,7 +46,7 @@ function init() {
     var addresses = ['Maruthamunai'];
 
     for (var x = 0; x < addresses.length; x++) {
-        $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address=maruthamunai+&key = AIzaSyBE2kpNzxI6BfWU606PwhaL9Lj29rDMpc4', null, function (data) {
+        $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address=maruthamunai&key=AIzaSyBE2kpNzxI6BfWU606PwhaL9Lj29rDMpc4', null, function (data) {
 
             // https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY
             console.log(data);
